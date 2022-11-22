741bd8179ea7672dff8140a787c195d3aba60f7e
islaiter <45145344+islaiter@users.noreply.github.com>
2022-11-22 13:06:10 +0100

    Create configuracionRed.sh
    Creamos un archivo que pide por teclado la direccion IP estática en formato CIDR, puerta de enlaces y DNS. Con ello configuramos el archivo netplan que se encuentra en /etc/netplan lo guardamos y lo aplicamos.

---
+++ configuracionRed.sh
@@ -0,0 +1,36 @@
+echo “Comenzamos, configurando la red…”
+
+# Creamos un backup del archivo primero
+
+cp /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bk_`date +%Y%m%d%H%M`
+
+# Desactivamos el dhcp
+
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/01-netcfg.yaml
+
+# Obtenemos la informacion de la tarjeta de red
+
+nic=`ip addr | awk 'NR==7{print $2}'`
+
+# Pedimos por teclado la configuracion de red
+
+read -p "Introduce la direccion IP estatica en formato CIDR, ejemplo: 192.168.2.20/22: " staticip
+read -p "Introduce la IP de la puerta de enlace: " gatewayip
+read -p "Introduce la IP de los DNS (separados por comas si es mas de uno, ej: 8.8.8.8,8.8.4.4): " nameserversip
+echo
+cat > /etc/netplan/01-netcfg.yaml <<EOF
+network:
+version: 2
+renderer: networkd
+ethernets:
+$nic
+addresses:
+- $staticip
+gateway4: $gatewayip
+nameservers:
+addresses: [$nameserversip]
+EOF
+sudo netplan apply
+echo “-----------------”
+echo “La configuracion de red se aplico correctamente”
+echo “-----------------”
